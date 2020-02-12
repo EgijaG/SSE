38232b2bbb8802a811be481c445aaf386a6f0cf1
EgijaG <egija.graudina@gmail.com>
2020-02-12 14:19:32 +0200

    Initial commit

---
+++ parts/header.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Media centre</title>
+
+</head>
+
+<body>
+    <header id="page-header">
+        <div class="top">
+            <div class="main-content-width centered padded cf">
+                <a href=""></a>
+                <div class="header-content">
+                    <div class="menu-wrapper">
+                        <div class="">
+                            <ul>
+                                <li><a href="./SSE/images/facebook.svg"></a></li>
+                                <li><a href="./SSE/images/instagram.svg"></a></li>
+                                <li><a href="./SSE/images/instagram.svg"></a></li>
+                                <li><a href="./SSE/images/youtube-symbol.svg"></a></li>
+                                <li><a href=""></a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <?php
