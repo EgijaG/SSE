0ee4c0186ed6c89d1ee620afdde8a034ac2ce416
EgijaG <egija.graudina@gmail.com>
2020-03-24 17:07:37 +0200

    lol

--- app/Views/Team/index.php
+++ app/Views/Team/index.php
@@ -1,1 +1,38 @@
 <h2>Team</h2>
+<div class="container">
+    <div class="card-group">
+        <div class="card border-dark">
+            <img src="./images/portraits/Sabine_Sile.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Sabīne Sīle-Eglīte</h5>
+                <h6>Director of the Centre for Media Studies</h6>
+                <h6>sabine.sile@sseriga.edu</h6>
+                <h6>+371 67015837</h6>
+                <p class="card-text">
+                    Responsible for management, programme development of the Centre for Media Studies programmes and events.</p>
+            </div>
+        </div>
+        <div class="card border-dark">
+            <img src="./images/portraits/Lasma_Antonevica.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Lāsma Antoneviča</h5>
+                <h6>Project Manager at the Centre for Media Studies</h6>
+                <h6> lasma.antonevica@sseriga.edu</h6>
+                <h6>+371 67015817</h6>
+                <p class="card-text">
+                    Responsible for administration and logistics of the Centre for Media Studies programmes and events.</p>
+            </div>
+        </div>
+        <div class="card border-dark">
+            <img src="./images/portraits/jana2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Jana Altenberga</h5>
+                <h6>Project Manager at the Centre for Media Studies</h6>
+                <h6>jana.altenberga@sseriga.edu</h6>
+                <h6>+371 28967641</h6>
+                <p class="card-text">
+                    Coordinates the Sustainable Media Business activities: The global media manager conference, Research, MiniMBA and accelerator programmes for media professionals.</p>
+            </div>
+        </div>
+    </div>
+</div>
